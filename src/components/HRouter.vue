5be78ed40eab8c841fe00217ae2f53226aeddbb9
Ali Al-Mahanna <alialmahanna@gmail.com>
2025-04-11 00:27:09 +0800

    Nav bar (no links) and home page set up

--- src/components/HRouter.vue
+++ src/components/HRouter.vue
@@ -1,3 +1,55 @@
-<template>
+<script setup lang="ts">
+//import { isAuthenticated as isAuthenticatedRef } from '../components/store.js';
+import { computed } from 'vue';
 
+const isAuthenticated = computed(() => isAuthenticatedRef.value);
+</script>
+
+<template>
+  <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><router-link to="/" class="nav-link" active-class="active"
+            aria-current="page">Home</router-link></li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="moreDropdown" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            Log In
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="moreDropdown">
+            <li>
+              <router-link to="/profile" class="dropdown-item">Users</router-link>
+            </li>
+            <li>
+              <router-link to="/settings" class="dropdown-item">Volunteers</router-link>
+            </li>
+
+          </ul>
+        </li>
+        <li class="nav-item"><router-link to="/community" class="nav-link" active-class="active">Community</router-link>
+        </li>
+        <li class="nav-item"><router-link to="/about" class="nav-link" active-class="active">About Us</router-link></li>
+        <!-- Dropdown Menu -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="moreDropdown" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            Resources
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="moreDropdown">
+            <li>
+              <router-link to="/profile" class="dropdown-item">Health Service</router-link>
+            </li>
+            <li>
+              <router-link to="/settings" class="dropdown-item">Legal Assistance</router-link>
+            </li>
+            <li>
+              <router-link to="/help" class="dropdown-item">Education</router-link>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item"><router-link to="/community" class="nav-link" active-class="active">Community</router-link>
+        </li>
+      </ul>
+    </header>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container mt-5 border py-5 rounded-5">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Welcome to Monash R and A Support!</h1>
+        <h2 class="text-center">Please select user type</h2>
+      </div>
+      <div class="border rounded-5 mx-auto col-8">
+        <div class="my-3">
+          <div class="row justify-content-center">
+            <button class="col-6 my-2 btn btn-dark">User Log In</button>
+          </div>
+          <div class="row justify-content-center">
+            <button class="col-6 my-2 btn btn-dark">Volunteer Log In</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
